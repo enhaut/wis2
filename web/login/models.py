a53911005984522adb69ab29af6c7ca073c6a135
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-10-14 11:47:26 +0200

    login: added users' roles

--- web/login/models.py
+++ web/login/models.py
@@ -7,9 +7,17 @@ class User(AbstractUser):
     username = models.CharField(primary_key=True, unique=True, verbose_name="xlogin", max_length=8)
     email = models.EmailField("mail", unique=True)
 
+    ROLES = [
+        ("R", "Registred user"),
+        ("S", "Student"),
+        ("T", "Teacher"),
+        ("G", "Guarantor"),
+        ("A", "Administrator")
+    ]
+    role = models.CharField(max_length=1, choices=ROLES, default=ROLES[0][0])
+
     # disable unused default attributes
     id = None
-    last_login = None
     is_superuser = None
     groups = None
     user_permissions = None
