db3703a3b2c976949afaafc6f29689e72f396187
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-10-16 22:19:54 +0200

    groups: replaced users' roles with groups

--- web/login/models.py
+++ web/login/models.py
@@ -7,19 +7,9 @@ class User(AbstractUser):
     username = models.CharField(primary_key=True, unique=True, verbose_name="xlogin", max_length=8)
     email = models.EmailField("mail", unique=True)
 
-    ROLES = [
-        ("R", "Registred user"),
-        ("S", "Student"),
-        ("T", "Teacher"),
-        ("G", "Guarantor"),
-        ("A", "Administrator")
-    ]
-    role = models.CharField(max_length=1, choices=ROLES, default=ROLES[0][0])
-
     # disable unused default attributes
     id = None
     is_superuser = None
-    groups = None
     user_permissions = None
     is_staff = None
     date_joined = None
