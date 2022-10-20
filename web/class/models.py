91e2abfe06525a87fa2e394367c7c77ebd1f41af
belticek17 <belticek@gmail.com>
2022-10-20 20:09:56 +0200

    resolved requested changes

--- web/class/models.py
+++ web/class/models.py
@@ -5,8 +5,14 @@ from course.models import Course
 # Create your models here.
 
 
+class TypeOfClass(models.Model):
+    shortcut = models.CharField(max_length=3)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+
 class Class(models.Model):
     courses_class = models.ForeignKey(Course, on_delete=models.CASCADE)
+    type_of_class = models.ForeignKey(TypeOfClass, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     date = models.DateTimeField()
@@ -17,25 +23,3 @@ class Assessment(models.Model):
     class_assessment = models.ForeignKey(Class, on_delete=models.CASCADE)
     point_evaluation = models.IntegerField()
     published_date = models.DateTimeField()
-
-class TypeOfClass(models.Model):
-    LECTURE = 'LEC'
-    PRACTICE = 'PRA'
-    DEMO_PRACTICE = 'DPR'
-    EXAM = 'EXM'
-    HALF_TERM_EXAM = 'HTX'
-    PROJECT = 'PRO'
-    CREDIT = 'CRE'
-
-    types_of_classes_choices = [
-        (LECTURE, 'Lecture'),
-        (PRACTICE, 'Practice'),
-        (DEMO_PRACTICE, 'Demo-practice'),
-        (EXAM, 'Exam'),
-        (HALF_TERM_EXAM, 'Half-term exam'),
-        (PROJECT, 'Project'),
-        (CREDIT, 'Credit'),
-    ]
-
-    types_of_classes = models.CharField(max_length=3, choices=types_of_classes_choices, default=LECTURE)
-    type_of_class = models.ForeignKey(Class, on_delete=models.CASCADE)
--- web/course/models.py
+++ web/course/models.py
@@ -4,35 +4,19 @@ sys.path.append('..')
 from login.models import User
 
 
+class TypeOfCourse(models.Model):
+    shortcut = models.CharField(max_length=3)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+
 class Course(models.Model):
+    type_of_course = models.ForeignKey(TypeOfCourse, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     price = models.IntegerField()
     limit_of_registered = models.IntegerField()
 
-
-
 class CourseUpdate(models.Model):
     course_update_id = models.ForeignKey(Course, on_delete=models.CASCADE)
     description = models.CharField(max_length=100)
     date = models.DateTimeField()
-
-class TypeOfCourse(models.Model):
-    COMPULSORY = 'C'
-    ELECTIVE = 'E'
-    COMPULSORY_OPTIONAL = 'C-O'
-
-    types_of_courses_choices = [
-        (COMPULSORY, 'Compulsory'),
-        (ELECTIVE, 'Elective'),
-        (COMPULSORY_OPTIONAL, 'Compulsory-optional'),
-    ]
-
-
-    types_of_courses = models.CharField(max_length=3, choices=types_of_courses_choices, default=COMPULSORY)
-    type_of_course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-
-
-    
-
