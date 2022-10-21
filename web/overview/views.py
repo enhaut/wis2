cc26458103e3f6e51384db0ccc6edd44895b9e36
belticek17 <belticek@gmail.com>
2022-10-21 16:18:20 +0200

    user page views and basic funcionality

--- web/overview/views.py
+++ web/overview/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from django.views.generic import View
@@ -8,5 +9,27 @@ class OverviewView(LoginRequiredMixin, View):
     template_name = "overview.html"
 
     def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Student').exists():
+            return HttpResponseRedirect('/student')
+        else:
+            return HttpResponseRedirect('/employee')
+
+class StudentView(LoginRequiredMixin, View):
+    template_name = "student.html"
+
+    #group_required = u"Administrator"
+    #redirect_unauthenticated_users = False
+    #raise_exception = True
+
+    def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
+class EmployeeView(LoginRequiredMixin, View):
+    template_name = "employee.html"
+
+    #group_required = u"Administrator"
+    #redirect_unauthenticated_users = False
+    #raise_exception = True
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
