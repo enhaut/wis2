9f07ebfd30b518588b7c46e982d1c2f85629d189
Ch1L1 <tomas.homola123@gmail.com>
2022-10-22 15:44:07 +0200

    Added admin page view

--- web/admin/views.py
+++ web/admin/views.py
@@ -1,6 +1,19 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import View
 from braces.views import GroupRequiredMixin
+
+
+class AdministratorView(GroupRequiredMixin, View):
+    template_name = "administrator.html"
+
+    group_required = u"Administrator"
+    redirect_unauthenticated_users = False
+    raise_exception = True
+
+    def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Admin').exists():
+            return HttpResponseRedirect('/admin')
 
 
 class AdminView(GroupRequiredMixin, View):
@@ -12,4 +25,3 @@ class AdminView(GroupRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-
