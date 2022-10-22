dab1403b4375345d54b044e08e6286b24337c291
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-10-22 19:01:40 +0200

    Course: fixed links

--- web/course/urls.py
+++ web/course/urls.py
@@ -3,5 +3,6 @@ from . import admin as a
 
 
 urlpatterns = [
-    path('course/admin/', a.CourseAdminView.as_view(), name="course_admin")
+    path('admin/course', a.CourseAdminView.as_view(), name="course_admin"),
+    path('admin/course/<slug:id>', a.EditCourseView.as_view(), name="edit_course")
 ]
