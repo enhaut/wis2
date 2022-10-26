2a95ed2f02591f1af111435365cdbd2d1e4ef8a1
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-10-26 15:38:29 +0200

    classes: classes view for teacher

--- web/class/admin.py
+++ web/class/admin.py
@@ -2,8 +2,11 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import View
 from braces.views import GroupRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+
 
 import sys
+from . import models
 sys.path.append('..')
 from course.models import Course
 
@@ -16,10 +19,36 @@ class CoursesView(GroupRequiredMixin, View):
     raise_exception = True
 
     def _get_courses(self, request):
-        courses = Course.objects.filter(
-            Q(lectors=request.user) | Q(guarantor=request.user)
-        )
+        try:
+            courses = Course.objects.filter(
+                Q(lectors=request.user) | Q(guarantor=request.user)
+            )
+        except ObjectDoesNotExist:
+            return []
+
         return courses
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {"courses": self._get_courses(request)})
+
+
+class ClassView(GroupRequiredMixin, View):
+    template_name = "class/admin/classes.html"
+
+    group_required = [u"Guarantor", u"Teacher"]
+    redirect_unauthenticated_users = False
+    raise_exception = True
+
+    def _get_classes(self, request, id):
+        try:
+            classes = models.Class.objects.filter(
+                courses_class=id
+            )
+        except ObjectDoesNotExist:
+            return []
+
+        return classes
+
+    def get(self, request, id, *args, **kwargs):
+        return render(request, self.template_name, {"classes": self._get_classes(request, id)})
+
--- web/class/urls.py
+++ web/class/urls.py
@@ -4,4 +4,5 @@ from . import admin
 
 urlpatterns = [
     path('admin/class', admin.CoursesView.as_view(), name="admin_classes"),
+    path('admin/class/<slug:id>', admin.ClassView.as_view(), name="view_classes"),
 ]
