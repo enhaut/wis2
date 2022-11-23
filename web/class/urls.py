32f4b9a79a40fb941ff9b460d8103c1377edd42f
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-11-23 15:32:09 +0100

    classes: registration

--- web/class/urls.py
+++ web/class/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import admin
+from . import views
 
 
 urlpatterns = [
+    path('class/registration', views.ClassesRegistrationView.as_view(), name="class_registration"),
+    path('class/detail/<int:class_id>', views.ClassDetailView.as_view(), name="class_detail"),
     path('admin/class', admin.CoursesView.as_view(), name="admin_classes"),
     path('admin/class/<slug:id>', admin.ClassView.as_view(), name="view_classes"),
     path('admin/class/<slug:id>/<int:class_id>', admin.EditClassView.as_view(), name="edit_class"),
