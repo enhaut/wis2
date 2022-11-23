e8fe3af09a567aff947da8a511dcd5a767d8d7f5
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-11-23 12:29:54 +0100

    classes: added support of occuring events

--- web/class/urls.py
+++ web/class/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/class', admin.CoursesView.as_view(), name="admin_classes"),
     path('admin/class/<slug:id>', admin.ClassView.as_view(), name="view_classes"),
     path('admin/class/<slug:id>/<int:class_id>', admin.EditClassView.as_view(), name="edit_class"),
+    path('admin/class/<slug:id>/<int:class_id>/<int:date_id>/remove', admin.RemoveClassDatesView.as_view(), name="remove_date"),
     path('admin/class/<slug:id>/<int:class_id>/registrations', admin.RegistrationSettingsView.as_view(), name="class_reg"),
 ]
