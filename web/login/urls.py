13a035dad87fb5068f2c7af6a6f8a5634e53f912
Tomáš Homola <80128605+Ch1L1@users.noreply.github.com>
2022-11-26 19:37:08 +0100

    Added view for unregistered users (#80)
    * Added view for unregistered users
    
    * Added correct redirects for course content

--- web/login/urls.py
+++ web/login/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.LoginPageView.as_view(), name="loginpage"),
     path('do_login', views.LoginView.as_view(), name="login"),
     path('logout', views.LogoutView.as_view(), name="logout"),
+    path('courses', views.AvailableCoursesView.as_view(), name="non_registered"),
+    path('course/<slug:shortcut>', views.CourseContentView.as_view(), name="course_content"),
     path('edit_profile', views.EditProfileView.as_view(), name="edit_profile")
 ]
